9c843a1b003996718602847c14a704990ea7a559
Adeini <adenilson.ini@gmail.com>
2023-11-05 11:04:04 -0300

    criacao do primeiro codigo

--- src/ConsoleApp1/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Informe seu Nome ?");
+
+
+var nome = Console.ReadLine();
+
+Console.WriteLine("Informe a idade ?");
+
+var idade = Convert.ToInt32(Console.ReadLine());
+
+if (nome == "joao" || idade >= 18)
+{
+     Console.WriteLine($"Seu nome é: {nome} você é maior de idade");
+} else
+{
+    Console.WriteLine("O nome digitado não é joao !");
+}
+
